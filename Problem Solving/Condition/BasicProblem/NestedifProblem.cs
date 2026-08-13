b3d64d26289b01e6a6e108f3ec4040c9657d084b
Rashed Ahommod <138321005+rashedahmed0@users.noreply.github.com>
2026-08-13 10:00:12 +0600

    Nested if Problem

---
+++ Problem Solving/Condition/BasicProblem/NestedifProblem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Problem_Solving.Condition.BasicProblem
+{
+    public class NestedifProblem
+    {
+        public void ATMWithdrawal()
+        {
+            int pin = 1234;
+            int suficientBalance = 1000;
+            if (pin == 1234) {
+                if(suficientBalance > 500)
+                {
+                    Console.WriteLine("withdra success your balacne id : {0} " , suficientBalance );
+                }
+                else
+                {
+                    Console.WriteLine("withdra Failed your balacne id : {0} \" , suficientBalance ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("withdra Failed your balacne id : {0} \" , suficientBalance ");
+            }
+        }
+
+
+        public void JobEligibility()
+        {
+            int age = 26;
+            string education = "bachelor";
+            int experience = 2;
+            if (age >= 18)
+            {
+                if(education == "bachelor")
+                {
+                    if(experience >1)
+                    {
+                        Console.WriteLine("Your are eligible for job Your age {0} , educaiotn {1} , experience {2} ", age, education, experience);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your are not eligible for job Your age {0} , educaiotn {1} , experience {2} ", age, education, experience);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your are not eligible for job Your age {0} , educaiotn {1} , experience {2} ", age, education, experience);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your are not eligible for job Your age {0} , educaiotn {1} , experience {2} ", age, education, experience);
+            }
+        }
+
+
+        public void OnlineShopping()
+        {
+            int productPrice = 500 ;
+            bool ismember = true;
+            string coupon = "2213";
+            if(productPrice > 499)
+            {
+                if(ismember == true)
+                {
+                    if (coupon == "2213")
+                    {
+                        Console.WriteLine("you got discount ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("you got discount ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("you got discount ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("you got discount ");
+            }
+        }
+
+
+
+    }
+}
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0bd66d86fe347b88135a332da941efbdbbef06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e961cedecb974db5d1e32bfa11e89dcd727fa7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
