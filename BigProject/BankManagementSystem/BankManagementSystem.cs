230bde61b8c2b71f3ffaa3e3c0fc3a512880857a
Rashed Ahommod <138321005+rashedahmed0@users.noreply.github.com>
2026-09-02 19:44:51 +0600

    commit bank account mnage manet

--- BigProject/BankManagementSystem/BankManagementSystem.cs
+++ BigProject/BankManagementSystem/BankManagementSystem.cs
@@ -42,26 +42,158 @@ namespace ConsoleApp1.BigProject.BankManagementSystem
                 {
                     case 1:
                     Console.WriteLine(" Create Account ");
-                    break;
+
+                        Console.WriteLine("Enter Account ID: ");
+                        int accountId = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter name : ");
+                        string name = (Console.ReadLine());
+                        Console.WriteLine("Enter number : ");
+                        string number = (Console.ReadLine());
+                        Console.WriteLine("Enter account type : ");
+                        string accountType = (Console.ReadLine());
+                        Console.WriteLine("Enter balance : ");
+                        decimal balance = decimal.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter address : ");
+                        string address = (Console.ReadLine());
+
+                        Account obj = new Account();
+                        obj.AccountId = accountId;
+                        obj.Name = name;
+                        obj.Number = number; 
+                        obj.AccountType = accountType; 
+                        obj.Balance = balance; 
+                        obj.Address = address;
+
+                        accounts[accountCount] = obj;
+                        accountCount++;
+
+                        Console.WriteLine("Account created successfully!");
+
+
+
+
+                        break;
 
                     case 2:
                         Console.WriteLine("Show All Accounts");
+                        for (int i = 0;  i < accountCount; i++)
+                        {
+                            Account account = accounts[i];
+                            Console.WriteLine($"Account ID: {account.AccountId}, Name: {account.Name}, Number: {account.Number}, Account Type: {account.AccountType}, Balance: {account.Balance}, Address: {account.Address}");
+                        } 
+
                         break;
                     case 3:
                         Console.WriteLine("Search Account");
+                        Console.WriteLine("Enter Account ID to search: ");
+                        int accuuntIdToSearch = int.Parse(Console.ReadLine());
+                        for (int i = 0; i < accountCount; i++)
+                        {
+                            Account account = accounts[i];
+                            if (accuuntIdToSearch == account.AccountId)
+                            {
+                                Console.WriteLine($"Account ID: {account.AccountId}, Name: {account.Name}, Number: {account.Number}, Account Type: {account.AccountType}, Balance: {account.Balance}, Address: {account.Address}");
+                            }
+                            
+                        } 
+
                         break;
                     case 4:
                         Console.WriteLine("Deposit Money");
+                        Console.WriteLine("Enter Account ID to deposit money: ");
+                        int depositAccountId = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter amount to deposit: ");
+                        decimal depositAmount = decimal.Parse(Console.ReadLine());
+                        for (int i = 0; i < accountCount; i++)
+                        {
+                            Account account = accounts[i];
+                            if (depositAccountId == account.AccountId)
+                            {
+                                account.Balance += depositAmount;
+                            } 
+
+                        } 
                         break;
                     case 5:
                         Console.WriteLine("Withdraw Money");
+
+                        Console.WriteLine("enter withdrawal Id: ");
+                        decimal withdrawId = int.Parse(Console.ReadLine());
+                        Console.WriteLine("enter withdrawal amount: ");
+                        decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+                        for(int i = 0; i < accountCount; i++)
+                        {
+                            Account account = accounts[i];
+                            if (withdrawId == account.AccountId && withdrawalAmount <= account.Balance && account.Balance > 0 )
+                            {
+                                account.Balance -= withdrawalAmount; 
+                                Console.WriteLine("withdrawal successful");
+                            }
+
+                        }
                         break; 
                     case 6:
                         Console.WriteLine("Transfer Money");
+
+                        Console.WriteLine("Enter from Account ID: ");
+                        int fromAccountId = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter to Account ID: ");
+                        int toAccountId = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter transfer amount: ");
+                        decimal transferAmount = decimal.Parse(Console.ReadLine());
+
+                        for(int i = 0; i < accountCount; i++)
+                        {
+                            Account account = accounts[i]; 
+                            if(fromAccountId == account.AccountId)
+                            {
+                                if(transferAmount <= account.Balance && account.Balance > 0)
+                                {
+                                    account.Balance -= transferAmount;
+                                    Console.WriteLine("Transfer successful");
+                                }
+                            }
+                            if(toAccountId == account.AccountId)
+                            {
+                                account.Balance += transferAmount;
+                            }
+
+                        }
                         break; 
                     case 7:
                         Console.WriteLine("Update Account");
+
+                        Console.WriteLine("Enter Account ID to update: ");
+                        int accountIdToUpdate = int.Parse(Console.ReadLine());
+
+                        for(int i = 0; i < accountCount; i++)
+                        {
+                            Account account = accounts[i];
+                            if (accountIdToUpdate == account.AccountId)
+                            {
+                                Console.WriteLine("Enter name : ");
+                                 name = (Console.ReadLine());
+                                Console.WriteLine("Enter number : ");
+                                 number = (Console.ReadLine());
+                                Console.WriteLine("Enter account type : ");
+                                 accountType = (Console.ReadLine());
+                                Console.WriteLine("Enter address : ");
+                                 address = (Console.ReadLine());
+
+                                Account obj1 = new Account();
+                                obj1.Name = name;
+                                obj1.Number = number;
+                                obj1.AccountType = accountType;
+                                obj1.Address = address;
+
+                                accounts[i] = obj1;
+
+                                Console.WriteLine("Account updated successfully!");
+                            }
+                        }
                         break; 
+
                     case 8:
                         Console.WriteLine("Delete Account");
                         break; 
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a030e3016f3f059af4c4c9e521e58c2683eccbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61021ff50aa3366db65b1340a544d84fc1fa2747")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
